e35b37446af54d358fb04e0ca1f800f20c446359
Christopher Makombe <125880140+christopher-codespaces@users.noreply.github.com>
2023-04-16 23:08:42 +0000

    Made the edits to fit the design will continue with the screen details page

--- components/SignUp.tsx
+++ components/SignUp.tsx
@@ -6,12 +6,7 @@ const SignUp = () =>{
       <div className="container  max-w-sm mx-auto flex-1 flex flex-col items-center justify-center px-2">
     <div className="bg-gray-300 px-6 py-8 rounded shadow-md text-black w-full">
       <h1 className="mb-8 text-3xl text-center font-bold">Sign up</h1>
-      <input 
-        type="text"
-        className="block border border-gray-300 w-full p-3 rounded mb-4"
-        name="fullname"
-        placeholder="Full Name" />
-
+      
       <input 
         type="text"
         className="block border border-gray-300 w-full p-3 rounded mb-4"
@@ -20,39 +15,26 @@ const SignUp = () =>{
 
       <input 
         type="password"
-        className="block border border-gray-300 w-full p-3 rounded mb-4"
+        className="block border border-gray-300 max-h-20	 w-full p-3                 rounded mb-4"
         name="password"
         placeholder="Password" />
 
-      <input 
-        type="password"
-        className="block border border-gray-300 w-full p-3 rounded mb-4"
-        name="confirm_password"
-        placeholder="Confirm Password" />
-
+      
       <button
         type="submit"
-        className="w-full text-center py-3 rounded bg-green-500 text-white hover:bg-green-600 focus:outline-none my-1"
-      >Create Account</button>
+        className="w-full text-center py-3 rounded bg-green-500   focus:outline-none my-1 text-white	bg-black"
+      > Login   </button>
 
       <div className="text-center text-sm text-gray-600 mt-4">
-        By signing up, you agree to the 
-        <a className="no-underline border-b border-gray-600 text-gray-600" href="#">
-          Terms of Service
-        </a> and 
-        <a className="no-underline border-b border-gray-600 text-gray-600" href="#">
-          Privacy Policy
-        </a>
+        Do not have an account? 
+        <span className="no-underline   text-gray-   font-bold   "                     href="#">
+        Create account         
+        </span> 
+       
       </div>
 
     </div>
 
-    <div className="text-gray-600 mt-6">
-      Already have an account? 
-      <a className="no-underline border-b border-blue-500 text-blue-500" href="../login/">
-        Log in
-      </a>.
-    </div>
 
   </div>
 </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,14 +3,13 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import  SignUp from "../components/SignUp"
-import  LogIn from "../components/LogIn"
 
 
 const Home: NextPage = () => {
   return (
     <>
       
-      <LogIn/>
+      <SignUp/>
     </>
   )
 }
