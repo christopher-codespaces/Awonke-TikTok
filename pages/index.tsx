67c2b48fade7d966c2dbcca07ba9c18cbad7c682
christopher-codespaces <125880140+christopher-codespaces@users.noreply.github.com>
2023-04-18 13:47:28 +0000

    helllo world
    HEY

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,8 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import  SideBar from "../components/SideBar"
-import  Header from "../components/Header"
+import  Accounts from "../components/Accounts"
+import  SignUp from "../components/SignUp"
 import  Add from "../components/Add"
 import  Other from "../components/Other"
 
@@ -12,10 +12,10 @@ import  Other from "../components/Other"
 const Home: NextPage = () => {
   return (
     <>
-       <Header/>
-      <SideBar/>
+       <Accounts/>
+{/*       <SideBar/>
       <Other/>
-      <Add/>
+      <Add/> */}
     </>
   )
 }
